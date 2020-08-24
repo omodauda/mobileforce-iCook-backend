7c4bca69142284247529da1c653648a41f336c8a
Dauda Lawal <omodauda.dl@gmail.com>
2020-08-24 15:11:08 +0100

    implement redis cache

--- Helpers/redis.js
+++ Helpers/redis.js
@@ -1,6 +1,6 @@
 const redis = require('redis');
 
-const REDIS_PORT = process.env.REDIS_URL||6379;
+const REDIS_PORT = process.env.REDISCLOUD_URL||6379;
 
 const client = redis.createClient(REDIS_PORT);
 
